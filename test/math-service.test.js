de1d6d86cb01b35007e28b47a42ccab29b6b7dce
Kristian Mandrup <kmandrup@gmail.com>
2017-04-13 20:20:50 +0200

    mountebank test graph service api

--- test/math-service.test.js
+++ test/math-service.test.js
@@ -1,50 +1,37 @@
+// SEE: https://www.npmjs.com/package/mountebank-helper
+
 // import the mountebank helper library
 const mbHelper = require('mountebank-helper');
 
 // create the skeleton for the imposter (does not post to MB)
-const firstImposter = new mbHelper.Imposter({
+const graphImposter = new mbHelper.Imposter({
   'imposterPort': 3000
 });
 
 // construct sample responses and conditions on which to send it
-const sample_response = {
-  'uri': '/hello',
-  'verb': 'GET',
+const graph_response = {
+  'uri': '/api/graph',
+  'verb': 'POST',
   'res': {
     'statusCode': 200,
     'responseHeaders': {
       'Content-Type': 'application/json'
     },
+    // YAML response expected
     'responseBody': JSON.stringify({
       'hello': 'world'
     })
   }
 };
 
-const another_response = {
-  'uri': '/pets/123',
-  'verb': 'PUT',
-  'res': {
-    'statusCode': 200,
-    'responseHeaders': {
-      'Content-Type': 'application/json'
-    },
-    'responseBody': JSON.stringify({
-      'somePetAttribute': 'somePetValue'
-    })
-  }
-};
-
-
 // add our responses to our imposter
-firstImposter.addRoute(sample_response);
-firstImposter.addRoute(another_response);
+graphImposter.addRoute(graph_response);
 
 // start the MB server  and post our Imposter to listen!
 mbHelper.startMbServer(2525)
   .then(function () {
-    firstImposter.postToMountebank()
-      .then(() => {
-        console.log('Imposter Posted! Go to http://localhost:3000/hello');
+    graphImposter.postToMountebank()
+      .then((res) => {
+        console.log('Imposter Posted!', res);
       });
   });
